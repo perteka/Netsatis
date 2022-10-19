8902e7a9d3ffed84a0a9ef0969b50e0765807810
AliHanPertek <alihan.pertek@hotmail.com>
2022-10-20 01:48:48 +0300

    .

---
+++ NetSatis.Entities/Tables/Fis.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSatis.Entities.Tables
+{
+    public class Fis
+    {
+        public int Id { get; set; }
+        public string FisKodu { get; set; }
+        public string FisTuru { get; set; }
+        public string CariTuru { get; set; }
+        public string Carikodu { get; set; }
+        public string CariAdi { get; set; }
+        public string BelgeNo { get; set; }
+        public DateTime Tarih { get; set; }
+        public string PlasiyerKodu { get; set; }
+        public string PlasiyerAdi { get; set; }
+        public decimal IskontoOrani { get; set; }
+        public decimal IskontoTutar { get; set; }
+        public string Aciklama { get; set; }
+
+    }
+}
