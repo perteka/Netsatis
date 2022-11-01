3a76a15194ffc413f9082e5f3762dad74055c538
aliha <aliha@OMEN>
2022-11-01 13:37:52 +0300

    feature/6

---
+++ NetSatis.Entities/Mapping/FisMap.cs
@@ -0,0 +1,47 @@
+﻿using NetSatis.Entities.Tables;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSatis.Entities.Mapping
+{
+    public class FisMap : EntityTypeConfiguration<Fis>
+    {
+        public FisMap()
+        {
+            this.HasKey(p => p.Id);
+            this.Property(p => p.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            this.Property(p => p.FisKodu).HasMaxLength(30);
+            this.Property(p => p.FisTuru).HasMaxLength(30);
+            this.Property(p => p.CariTuru).HasMaxLength(30);
+            this.Property(p => p.Carikodu).HasMaxLength(30);
+            this.Property(p => p.CariAdi).HasMaxLength(30);
+            this.Property(p => p.BelgeNo).HasMaxLength(30);
+            //this.Property(p => p.Tarih).has(30);
+            this.Property(p => p.PlasiyerKodu).HasMaxLength(30);
+            this.Property(p => p.PlasiyerAdi).HasMaxLength(30);
+            this.Property(p => p.IskontoOrani).HasPrecision(12, 2);
+            this.Property(p => p.IskontoTutar).HasPrecision(5, 2);
+            this.Property(p => p.Aciklama).HasMaxLength(200);
+
+            this.ToTable("Fisler");
+            this.Property(p => p.Id).HasColumnName("Id");
+            this.Property(p => p.FisKodu).HasColumnName("FisKodu");
+            this.Property(p => p.FisTuru).HasColumnName("FisTuru");
+            this.Property(p => p.CariTuru).HasColumnName("CariTuru");
+            this.Property(p => p.Carikodu).HasColumnName("Carikodu");
+            this.Property(p => p.CariAdi).HasColumnName("CariAdi");
+            this.Property(p => p.BelgeNo).HasColumnName("BelgeNo");
+            this.Property(p => p.Tarih).HasColumnName("Tarih");
+            this.Property(p => p.PlasiyerKodu).HasColumnName("PlasiyerKodu");
+            this.Property(p => p.PlasiyerAdi).HasColumnName("PlasiyerAdi");
+            this.Property(p => p.IskontoOrani).HasColumnName("IskontoOrani");
+            this.Property(p => p.IskontoTutar).HasColumnName("IskontoTutar");
+            this.Property(p => p.Aciklama).HasColumnName("Aciklama");
+        }
+    }
+}
---
+++ NetSatis.Entities/Mapping/StokHareketMap.cs
@@ -0,0 +1,57 @@
+﻿using NetSatis.Entities.Tables;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSatis.Entities.Mapping
+{
+    class StokHareketMap:EntityTypeConfiguration<StokHareket>
+    {
+        public StokHareketMap()
+        {
+            this.HasKey(p => p.Id);
+            this.Property(p => p.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            this.Property(p => p.FisKodu).HasMaxLength(30);
+            this.Property(p => p.Hareket).HasMaxLength(30);
+            this.Property(p => p.StokKodu).HasMaxLength(30);
+            this.Property(p => p.StokAdi).HasMaxLength(30);
+            this.Property(p => p.BarkotTuru).HasMaxLength(30);
+            this.Property(p => p.Barkod).HasMaxLength(30);
+            this.Property(p => p.Birimi).HasMaxLength(30);
+            this.Property(p => p.Miktar).HasPrecision(5, 2);
+            this.Property(p => p.Kdv);
+            this.Property(p => p.BirimFiyati).HasPrecision(12, 2);
+            this.Property(p => p.IndirimOrani).HasPrecision(5 ,2);
+            this.Property(p => p.IndirimTutarı).HasPrecision(12, 2);
+            this.Property(p => p.ToplamTutar).HasPrecision(12, 2);
+            this.Property(p => p.DepoKodu).HasMaxLength(30);
+            this.Property(p => p.DepoAdi).HasMaxLength(30);
+            this.Property(p => p.SeriNo).HasMaxLength(30);
+            //this.Property(p => p.Tarih).HasMaxLength(30);
+            this.Property(p => p.Aciklama).HasMaxLength(200);
+
+            this.ToTable("StokHareketleri");
+            this.Property(p => p.Id).HasColumnName("Id");
+            this.Property(p => p.FisKodu).HasColumnName("FisKodu");
+            this.Property(p => p.Hareket).HasColumnName("Hareket");
+            this.Property(p => p.StokKodu).HasColumnName("StokKodu");
+            this.Property(p => p.StokAdi).HasColumnName("StokAdi");
+            this.Property(p => p.BarkotTuru).HasColumnName("BarkotTuru");
+            this.Property(p => p.Barkod).HasColumnName("Barkod");
+            this.Property(p => p.Birimi).HasColumnName("Birimi");
+            this.Property(p => p.Miktar).HasColumnName("Miktar");
+            this.Property(p => p.Kdv).HasColumnName("Kdv");
+            this.Property(p => p.BirimFiyati).HasColumnName("BirimFiyati");
+            this.Property(p => p.IndirimOrani).HasColumnName("IndirimOrani");
+            this.Property(p => p.IndirimTutarı).HasColumnName("IndirimTutarı");
+            this.Property(p => p.ToplamTutar).HasColumnName("ToplamTutar");
+            this.Property(p => p.DepoAdi).HasColumnName("DepoAdi");
+            this.Property(p => p.Tarih).HasColumnName("Tarih");
+            this.Property(p => p.Aciklama).HasColumnName("Aciklama");
+        }
+    }
+}
