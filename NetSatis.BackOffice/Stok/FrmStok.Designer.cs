a0e25cc0a68d033bf57df8fe805a41843a83e369
aliha <aliha@OMEN>
2022-12-09 19:13:02 +0300

    feaure/10

--- NetSatis.BackOffice/Stok/FrmStok.Designer.cs
+++ NetSatis.BackOffice/Stok/FrmStok.Designer.cs
@@ -29,33 +29,132 @@ namespace NetSatis.BackOffice.Stok
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmStok));
+            this.lblBaslik = new DevExpress.XtraEditors.LabelControl();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.btnEkle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDuzenle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnSıl = new DevExpress.XtraEditors.SimpleButton();
+            this.btnKopyala = new DevExpress.XtraEditors.SimpleButton();
+            this.btnGuncelle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnAra = new DevExpress.XtraEditors.SimpleButton();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // labelControl1
+            // lblBaslik
             // 
-            this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
-            this.labelControl1.Location = new System.Drawing.Point(339, 86);
-            this.labelControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(233, 45);
-            this.labelControl1.TabIndex = 0;
-            this.labelControl1.Text = "labelControl1wadawdawdawdawdawd";
+            this.lblBaslik.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBaslik.Appearance.Options.UseFont = true;
+            this.lblBaslik.Appearance.Options.UseTextOptions = true;
+            this.lblBaslik.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.lblBaslik.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.lblBaslik.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblBaslik.ImageOptions.Alignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblBaslik.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("lblBaslik.ImageOptions.Image")));
+            this.lblBaslik.Location = new System.Drawing.Point(0, 0);
+            this.lblBaslik.Margin = new System.Windows.Forms.Padding(4);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(1026, 57);
+            this.lblBaslik.TabIndex = 0;
+            this.lblBaslik.Text = "Stok Kartları";
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.btnAra);
+            this.groupControl1.Controls.Add(this.btnGuncelle);
+            this.groupControl1.Controls.Add(this.btnKopyala);
+            this.groupControl1.Controls.Add(this.btnSıl);
+            this.groupControl1.Controls.Add(this.btnDuzenle);
+            this.groupControl1.Controls.Add(this.btnEkle);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupControl1.Location = new System.Drawing.Point(0, 359);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(1026, 100);
+            this.groupControl1.TabIndex = 1;
+            this.groupControl1.Text = "Menü";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(12, 31);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(94, 57);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.Location = new System.Drawing.Point(112, 31);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(94, 57);
+            this.btnDuzenle.TabIndex = 2;
+            this.btnDuzenle.Text = "Düzenle";
+            // 
+            // btnSıl
+            // 
+            this.btnSıl.Location = new System.Drawing.Point(212, 31);
+            this.btnSıl.Name = "btnSıl";
+            this.btnSıl.Size = new System.Drawing.Size(94, 57);
+            this.btnSıl.TabIndex = 2;
+            this.btnSıl.Text = "Sil";
+            // 
+            // btnKopyala
+            // 
+            this.btnKopyala.Location = new System.Drawing.Point(312, 31);
+            this.btnKopyala.Name = "btnKopyala";
+            this.btnKopyala.Size = new System.Drawing.Size(94, 57);
+            this.btnKopyala.TabIndex = 2;
+            this.btnKopyala.Text = "Kopyala";
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(412, 31);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(94, 57);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "Güncelle";
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(512, 31);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(94, 57);
+            this.btnAra.TabIndex = 2;
+            this.btnAra.Text = "Ara";
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // FrmStok
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1026, 459);
-            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.lblBaslik);
             this.Name = "FrmStok";
             this.Text = "FrmStok";
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl lblBaslik;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.SimpleButton btnAra;
+        private DevExpress.XtraEditors.SimpleButton btnGuncelle;
+        private DevExpress.XtraEditors.SimpleButton btnKopyala;
+        private DevExpress.XtraEditors.SimpleButton btnSıl;
+        private DevExpress.XtraEditors.SimpleButton btnDuzenle;
+        private DevExpress.XtraEditors.SimpleButton btnEkle;
+        private System.Windows.Forms.ImageList ımageList1;
     }
 }
